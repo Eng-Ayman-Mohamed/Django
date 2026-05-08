0de4186604ed5d89b52c1817ab9944e438c0aa24
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-05-09 00:39:56 +0300

    css design using ai

--- ITIan/trainee/urls.py
+++ ITIan/trainee/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path('list/', views.traineeList, name="trainees"),
     path('<int:id>', views.viewTrainee, name="viewTrainee"),
-    path('add/', views.addTrainee),
+    path('add/', views.addTrainee, name="addTrainee"),
     path('update/<id>/', views.updateTrainee),
     path('delete/<int:id>/', views.deleteTrainee, name="deleteTrainee"),
 ]

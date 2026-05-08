18a37e9213aca23161e291dfb9e942537ca97634
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-05-08 18:05:43 +0300

    Add course and registration models with migrations
    - Created a new `course` model with fields: name, instructor, weight, startDate, endDate, and createdAt.
    - Removed the existing SQLite database file.
    - Added initial migrations for the `course` model.
    - Updated the `registration` app with necessary models, views, and URLs.
    - Generated __pycache__ files for all updated modules.

--- ITIan/course/models.py
+++ ITIan/course/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class course(models.Model):
+    name=models.CharField(max_length=20)
+    instructor=models.CharField(max_length=20) 
+    weight=models.IntegerField()
+    startDate=models.DateField()
+    endDate=models.DateField()
+    createdAt=models.DateField(auto_now_add=True)

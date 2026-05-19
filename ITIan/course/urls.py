f957192bb6632b2e005489504244e9962192b81b
Eng-Ayman-Mohamed <eng.ayman.mohamed.abotaha.kasim@gmail.com>
2026-05-19 23:41:08 +0300

    feat: Implement Course API with JWT authentication and create Course serializer

--- ITIan/course/urls.py
+++ ITIan/course/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
-from .import views
+# from django.urls import path
+# from .import views
 
-urlpatterns=[
-    path('list/', views.courseList),
-    path('add/', views.addCourse),
-    path('update/<id>/', views.updateCourse),
-    path('delete/<id>/', views.deleteCourse),
-]
+# urlpatterns=[
+#     path('list/', views.courseList),
+#     path('add/', views.addCourse),
+#     path('update/<id>/', views.updateCourse),
+#     path('delete/<id>/', views.deleteCourse),
+# ]
+
+from rest_framework.routers import DefaultRouter
+from .views import CourseView
+
+router = DefaultRouter()
+router.register(r'api/course', CourseView, basename='course')
+urlpatterns = router.urls
--- ITIan/course/views.py
+++ ITIan/course/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import course
+from rest_framework.viewsets import ModelViewSet
+from .serializer import CourseSerializer
+
+from rest_framework.permissions import IsAuthenticated
+
+
+class CourseView( ModelViewSet):
+    queryset = course.objects.all()
+    serializer_class = CourseSerializer
+
+    permission_classes = [IsAuthenticated]
+
+
 # Create your views here.
-
-def courseList(request):
-    courses = course.objects.all()
-    return render(request, 'course/list.html',{'courses':courses})
-
-def addCourse(request):
-    return HttpResponse('add Course')
-
-def updateCourse(request,id):
-    return HttpResponse(f'update Course with id: {id}')
-
-def deleteCourse(request,id):
-    return HttpResponse(f'delete Course with id: ${id}')
+# @login_required
+# def courseList(request):
+#     courses = course.objects.all()
+#     return render(request, 'course/list.html',{'courses':courses})
+# @login_required
+# def addCourse(request):
+#     return HttpResponse('add Course')
+# @login_required
+# def updateCourse(request,id):
+#     return HttpResponse(f'update Course with id: {id}')
+# @login_required
+# def deleteCourse(request,id):
+#     return HttpResponse(f'delete Course with id: ${id}')
